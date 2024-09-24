2a510cdee5755f6aa22184d9197767d4e3ca6299
mreshboboyev <mreshboboyev@gmail.com>
2024-09-24 10:31:28 +0500

    corrected return types and dtos names;

--- TaskManagementSystem.Application/DTOs/TaskDTO.cs
+++ TaskManagementSystem.Application/DTOs/TaskDTO.cs
@@ -2,7 +2,7 @@
 
 namespace TaskManagementSystem.Application.DTOs;
 
-public class TaskDto
+public class TaskDTO
 {
     public int Id { get; set; }
     public string Title { get; set; }
--- TaskManagementSystem.Application/Services/Interfaces/ITaskService.cs
+++ TaskManagementSystem.Application/Services/Interfaces/ITaskService.cs
@@ -5,27 +5,27 @@ namespace TaskManagementSystem.Application.Services.Interfaces;
 public interface ITaskService
 {
     // Create a new task
-    Task<TaskDto> CreateTaskAsync(CreateTaskDTO createTaskDTO);
+    Task<ResponseDTO<TaskDTO>> CreateTaskAsync(CreateTaskDTO createTaskDTO);
 
     // Update an existing task
-    Task<TaskDto> UpdateTaskAsync(int taskId, UpdateTaskDTO updateTaskDTO);
+    Task<ResponseDTO<TaskDTO>> UpdateTaskAsync(int taskId, UpdateTaskDTO updateTaskDTO);
 
     // Delete a task
     Task<bool> DeleteTaskAsync(int taskId);
 
     // Get a task by its ID
-    Task<TaskDto> GetTaskByIdAsync(int taskId);
+    Task<ResponseDTO<TaskDTO>> GetTaskByIdAsync(int taskId);
 
     // Get all tasks assigned to a specific user
-    Task<IEnumerable<TaskDto>> GetTasksByUserIdAsync(string userId);
+    Task<ResponseDTO<IEnumerable<TaskDTO>>> GetTasksByUserIdAsync(string userId);
 
     // Get all tasks associated with a project
-    Task<IEnumerable<TaskDto>> GetTasksByProjectIdAsync(int projectId);
+    Task<ResponseDTO<IEnumerable<TaskDTO>>> GetTasksByProjectIdAsync(int projectId);
 
     // Update the status of a task
-    Task<TaskDto> UpdateTaskStatusAsync(int taskId, TaskStatusDTO statusDto);
+    Task<ResponseDTO<TaskDTO>> UpdateTaskStatusAsync(int taskId, TaskStatusDTO statusDto);
 
     // Assign a task to a user
-    Task<TaskDto> AssignTaskToUserAsync(int taskId, string userId);
+    Task<ResponseDTO<TaskDTO>> AssignTaskToUserAsync(int taskId, string userId);
 }
 
