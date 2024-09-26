afe683d70552f10a7751a0b649a5ddc39f187436
mreshboboyev <mreshboboyev@gmail.com>
2024-09-26 20:05:26 +0500

    Adding Notificaition entity changes and migrations;

--- TaskManagementSystem.Infrastructure/Data/Migrations/20240926145630_updateNotificationEntityAndRelations.Designer.cs
+++ TaskManagementSystem.Infrastructure/Data/Migrations/20240926145630_updateNotificationEntityAndRelations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskManagementSystem.Infrastructure.Data;
@@ -11,9 +12,11 @@ using TaskManagementSystem.Infrastructure.Data;
 namespace TaskManagementSystem.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240926145630_updateNotificationEntityAndRelations")]
+    partial class updateNotificationEntityAndRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TaskManagementSystem.Infrastructure/Data/Migrations/20240926145630_updateNotificationEntityAndRelations.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagementSystem.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateNotificationEntityAndRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_UserId",
+                table: "Notifications");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Notifications",
+                newName: "SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notifications_UserId",
+                table: "Notifications",
+                newName: "IX_Notifications_SenderId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ActionTakenAt",
+                table: "Notifications",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientId",
+                table: "Notifications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Notifications",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Notifications",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_RecipientId",
+                table: "Notifications",
+                column: "RecipientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_RecipientId",
+                table: "Notifications",
+                column: "RecipientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_SenderId",
+                table: "Notifications",
+                column: "SenderId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_RecipientId",
+                table: "Notifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_SenderId",
+                table: "Notifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notifications_RecipientId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "ActionTakenAt",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Notifications");
+
+            migrationBuilder.RenameColumn(
+                name: "SenderId",
+                table: "Notifications",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notifications_SenderId",
+                table: "Notifications",
+                newName: "IX_Notifications_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_UserId",
+                table: "Notifications",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
