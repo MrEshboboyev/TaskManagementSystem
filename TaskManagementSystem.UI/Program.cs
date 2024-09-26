b2dfc3ab8156ff8d6b0b8bc48b284cc567f21fca
mreshboboyev <mreshboboyev@gmail.com>
2024-09-26 11:09:58 +0500

    adding authentication and pipeline , removed custom authentication with JWT;

--- TaskManagementSystem.UI/Program.cs
+++ TaskManagementSystem.UI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using TaskManagementSystem.Application.Mappings;
 using TaskManagementSystem.Infrastructure.Configurations;
 using TaskManagementSystem.Infrastructure.Services;
@@ -16,8 +17,6 @@ builder.Services.AddDatabaseConfiguration(builder.Configuration);
 // configure identity
 builder.Services.AddIdentityConfiguration();
 
-// configure JWT Authentication 
-builder.Services.AddJwtAuthentication(builder.Configuration);
 
 // configure lifetime for services
 builder.Services.AddApplicationServices(builder.Configuration);
@@ -25,6 +24,15 @@ builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 
 // configure automapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// adding authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromHours(10);
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Auth/AccessDenied";
+    });
 
 var app = builder.Build();
 
@@ -40,6 +48,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// adding pipeline for authentication
+app.UseAuthentication();
 
 app.UseAuthorization();
 
