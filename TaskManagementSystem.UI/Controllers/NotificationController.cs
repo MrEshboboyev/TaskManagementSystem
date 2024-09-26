3c3613185ea65460d995287fe13d755e1d1d78d1
mreshboboyev <mreshboboyev@gmail.com>
2024-09-26 21:45:52 +0500

    Notification controller/Index endpoint (view) writed (for receiving notifications);

---
+++ TaskManagementSystem.UI/Controllers/NotificationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TaskManagementSystem.Application.Services.Interfaces;
+
+namespace TaskManagementSystem.UI.Controllers
+{
+    [Authorize]
+    public class NotificationController(INotificationService notificationService) : Controller
+    {
+        private readonly INotificationService _notificationService = notificationService;
+
+        #region Private Methods
+        private string GetUserId() => User.FindFirstValue(ClaimTypes.NameIdentifier)
+            ?? throw new Exception("Login required!");
+        #endregion
+
+
+        public async Task<IActionResult> Index()
+        {
+            var userNotifications = await _notificationService.GetNotificationsForUserAsync(GetUserId());
+            return View(userNotifications.Data);
+        }
+
+
+    }
+}
