3b148032c5a70da28bb6e2350da58952fe5ffc9b
mreshboboyev <mreshboboyev@gmail.com>
2024-09-26 14:59:30 +0500

    Company CRUD views and endpoints;

--- TaskManagementSystem.UI/Areas/Boss/Controllers/CompanyController.cs
+++ TaskManagementSystem.UI/Areas/Boss/Controllers/CompanyController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Drawing.Printing;
 using System.Security.Claims;
 using TaskManagementSystem.Application.Common.Utility;
 using TaskManagementSystem.Application.DTOs.Company;
@@ -32,6 +31,9 @@ namespace TaskManagementSystem.UI.Areas.Boss.Controllers
         [HttpPost]
         public async Task<IActionResult> CompanyCreate(CompanyCreateDTO companyCreateDTO)
         {
+            // assign userId
+            companyCreateDTO.OwnerId = GetUserId();
+
             var result = await _companyService.CreateCompany(companyCreateDTO);
 
             if (!result.Success)
@@ -44,6 +46,8 @@ namespace TaskManagementSystem.UI.Areas.Boss.Controllers
             return RedirectToAction(nameof(CompanyIndex));
         }
 
+
+
         [HttpGet]
         public async Task<IActionResult> CompanyUpdate(int companyId)
         {
@@ -52,12 +56,55 @@ namespace TaskManagementSystem.UI.Areas.Boss.Controllers
             return View(company.Data);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CompanyUpdate(CompanyDTO companyDTO)
+        {
+            CompanyUpdateDTO companyUpdateDTO = new()
+            {
+                OwnerId = GetUserId(),
+                Description = companyDTO.Description,
+                Name = companyDTO.Name
+            };
+
+            var result = await _companyService.UpdateCompany(companyDTO.Id, companyUpdateDTO);
+
+            if (!result.Success)
+            {
+                TempData["error"] = result.Message;
+                return View(companyDTO);
+            }
+
+            TempData["success"] = "Company successfully updated";
+            return RedirectToAction(nameof(CompanyIndex));
+        }
+
         [HttpGet]
         public async Task<IActionResult> CompanyDelete(int companyId)
         {
             var company = await _companyService.GetCompanyAsync(companyId);
 
             return View(company.Data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CompanyDelete(CompanyDTO companyDTO)
+        {
+            CompanyDeleteDTO companyDeleteDTO = new()
+            {
+                OwnerId = GetUserId(),
+                CompanyId = companyDTO.Id
+            };
+
+            var result = await _companyService.DeleteCompanyAsync(companyDeleteDTO);
+
+            if (!result.Success)
+            {
+                TempData["error"] = result.Message;
+                return View(companyDTO);
+            }
+
+            TempData["success"] = "Company successfully deleted";
+            return RedirectToAction(nameof(CompanyIndex));
         }
     }
 }
